c8f564b8b68628478bb872ad904be91474a37164
Scottrock <andy@scottrockcafe.net>
2019-06-30 17:23:28 +0100

    Update zcash-cli.js

--- zcash-cli.js
+++ zcash-cli.js
@@ -663,7 +663,8 @@ function proc_zcash_cli_listunspent(data) {
         }));
         // generate html for balances
         //var html = "<b>" + taddr_balances[name] + "</b> <small>"+symbol+"</small> &rarr; <i><small>t-addr:</small> " + name + "</i><br />";
-        var html = "<b>" + taddr_balances[name] + "</b> <small>"+symbol+"</small> &rarr;" + " segid: " + taddr_segids[name] + "</small> &rarr;" + " <i><small>t-addr:</small> " + name + "</i><br />";
+        //var html = "<b>" + taddr_balances[name] + "</b> <small>"+symbol+"</small> &rarr;" + " segid: " + taddr_segids[name] + "</small> &rarr;" + " <i><small>t-addr:</small> " + name + "</i><br />";
+        var html = "<b>" + taddr_balances[name] + "</b> <small>" + symbol + "</small> &rarr;" + " segid: " + taddr_segids[name] + "</small> &rarr;" + " UTXO's: " + taddr_UTXO[name] + "</small> &rarr;" + " <i><small>t-addr:</small> " + name + "</i><br />";
         zcash_shield_taddresses_info.append(html);
         zcash_balance_taddresses_info.append(html);
     }
